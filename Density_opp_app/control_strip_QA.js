b43335ab4d4f04499ea2898bef3ff87a642f9372
Tyler Dean <tyler.adean93@yahoo.com>
2023-04-11 22:27:53 -0400

    getting stuff put together. no js yet

---
+++ Density_opp_app/control_strip_QA.js
@@ -0,0 +1,8 @@
+const QACSnuclear = document.getElementById("QACSnuclear")
+const QACSstartStation = document.getElementById("QACSstartStation")
+const QACSendStation = document.getElementById("QACSendStation")
+
+/* constName.addEventListener("change", () => {
+    whatever you want the function to do.. 
+    
+})  */
